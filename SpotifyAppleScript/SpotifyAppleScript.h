8c9480186a74ccdcf532b7c53b06b3f62f432053
miklos <m.kristyan@gmail.com>
2017-10-08 16:39:31 +0200

    Spotify renamed to SpotifyAppleScript

---
+++ SpotifyAppleScript/SpotifyAppleScript.h
@@ -0,0 +1,19 @@
+//
+//  Spotify.h
+//  Spotify
+//
+//  Created by Miklós Kristyán on 2017. 02. 11..
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Spotify.
+FOUNDATION_EXPORT double SpotifyVersionNumber;
+
+//! Project version string for Spotify.
+FOUNDATION_EXPORT const unsigned char SpotifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spotify/PublicHeader.h>
+
+
